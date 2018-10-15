8f916d25ec51bb25e03ba5f4e6568a352ac050ce
a w <antoine.wittmann@hotmail.fr>
2018-10-15 17:58:59 +0200

    correction helico

--- src/Classes/Helicoptere.php
+++ src/Classes/Helicoptere.php
@@ -2,9 +2,8 @@
 
 class Helicoptere {
 
-    //coordonnees en x y z
     public $x,$y,$z;
-    //direction vers laquelle est tournee l'helico
+    //direction we're facing
     public $direction;
 
     function __construct($posx,$posy) {
@@ -13,45 +12,27 @@ class Helicoptere {
         $this->$direction=0;
     }
 
-    function seDeplacer($distance) {
+    function move($distance) {
         $angle=($direction % 360);
         switch ($angle) {
             //si tout droit avance en y
             case 0:
-                if(($this->$y+$distance)>=12){
-                    $this->$y=12;
-                }else{
-                    $this->$y+=$distance;
-                }
+                $this->moveStraight($y,$distance,12,'up');
             break;
             //si vers la droite avance en x
             case 90:
-                if(($this->$x+$distance)>=20){
-                    $this->$x=20;
-                }else{
-                    $this->$x+=$distance;
-                }
+                $this->moveStraight($x,$distance,20,'up');
             break;
             //si vers le bas recule en y
             case 180:
-                if(($this->$y-$distance)<=0){
-                    $this->$y=0;
-                }else{
-                    $this->$y-=$distance;
-                }
-            break;
+                $this->moveStraight($y,$distance,0,'down');
             //si versla gauche recule en x
             case 270:
-                if(($this->$x-$distance)<=0){
-                    $this->$x=0;
-                }else{
-                    $this->$x-=$distance;
-                }
+                $this->moveStraight($y,$distance,0,'down');
             break;
             //si diagonale haut droite avance y et x
             case 45:
                 //il faut vérifier si on peut avancer àchaque fois
-                $resDistance=$distance;
                 while($distance>0){
                     if(($this->$x<20)&&($this->$y<12)){
                         $this->$x+=1;
@@ -62,8 +43,6 @@ class Helicoptere {
             break;
             //si diagonale bas droite recule y et avance x
             case 135:
-                //il faut vérifier si on peut avancer àchaque fois
-                $resDistance=$distance;
                 while($distance>0){
                     if(($this->$x<20)&&($this->$y>0)){
                         $this->$x+=1;
@@ -74,8 +53,6 @@ class Helicoptere {
             break;
             //si diagonale bas gauche recule y et x
             case 225:
-                //il faut vérifier si on peut avancer àchaque fois
-                $resDistance=$distance;
                 while($distance>0){
                     if(($this->$x>0)&&($this->$y>0)){
                         $this->$x-=1;
@@ -86,8 +63,6 @@ class Helicoptere {
             break;
             //si diagonale haut gauche avance y et recule x
             case 315:
-                //il faut vérifier si on peut avancer àchaque fois
-                $resDistance=$distance;
                 while($distance>0){
                     if(($this->$x>0)&&($this->$y<12)){
                         $this->$x-=1;
@@ -97,33 +72,45 @@ class Helicoptere {
                 }
             break;
             default:
-            //tant pis
-            printf("Erreur, angle non valable (très content)");
+                throw new Exception("L'angle ne correspond pas");
             break;
         }
     }
 
-    function seReorienter($angle){
+    function moveStraight($axis,$distance,$limit,$direction){
+        if($direction=='up'){
+            if(($this->$axis+$distance)>=$limit){
+                $this->$axis=$limit;
+            }else{
+                $this->$axis+=$distance;
+            }
+        }else{
+            if(($this->$axis+$distance)<=$limit){
+                $this->$axis=$limit;
+            }else{
+                $this->$axis-=$distance;
+            }
+        }
+    }
+
+    function changeDirection($angle){
         $this->$direction+=$angle;
     }
 
-    function prendreDegats(){
-        if($this->$z > 0) {
-            $this->$z-=1;
+    function takeDamage(){
+        if($this->$z <= 0) {
+            throw new Exception("Hélico déjà crash");
         }
+        $this->$z-=1;
     }
 
-    function estMort(){
-        if($this->$z == 0) {
-            return true;
-        }else{
-            return false;
-        }
+    function isDead(){
+        return $this->$z == 0 ;
     }
 
-    function attaquer($cible){
+    function attack($target){
         //chance de toucher : différence de niveau + distance
-        $proba = ($this->$z - $cible->$z) + abs($this->$x - $cible->$x) + abs($this->$y - $cible->$y); 
+        $proba = ($this->$z - $target->$z) + abs($this->$x - $target->$x) + abs($this->$y - $target->$y); 
         $jet = rand(1,6);
         if($jet > $proba){
             return true;
