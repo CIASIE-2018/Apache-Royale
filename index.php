b5d8975ae69ed7661dd14b0d6dbd4832b1311909
Jordan <jordanjezequel@bbox.fr>
2018-10-15 17:56:31 +0200

    correction form

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once 'vendor/autoload.php';
 
 session_start();
+
 $app = new \Slim\Slim();
 
 $loader = new Twig_Loader_Filesystem('src/Twig');
@@ -13,12 +14,12 @@ $app->get('/',function () use ($twig, $app){
     $g = new \apache\Controler\ControlerGeneral();
     $g->showIndex();
     */
-
+    echo $_COOKIE["PHPSESSID"];
     echo $twig->render('index.html', array('join' => $app->urlFor('join')));
 })->name('home');
 
 $app->post('/',function () {
-    
+    echo "sdnfkn";
 })->name('homeP');
 
 $app->get('/join',function () use ($twig){
