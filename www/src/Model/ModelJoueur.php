0f0008d2736f5a5c49ee76e5d557bb01fa76bb7a
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-10-24 09:27:54 +0200

    Modification ModelJoueur

--- www/src/Model/ModelJoueur.php
+++ www/src/Model/ModelJoueur.php
@@ -13,29 +13,44 @@ class ModelJoueur extends \Illuminate\Database\Eloquent\Model
         $this->game->addPlayer($this);
         //si joueur 1 helico initialise en bas, sinon en haut du plateau
         if($this->game->isPlayer1($this)){
-            $this->helicopteres=[new Helicoptere(5,0,0),new Helicoptere(10,0,0),new Helicoptere(15,0,0)];
+            
+            $this->h1 = new ModelHelicoptere(5,0,0);
+            $this->h2 = new ModelHelicoptere(10,0,0);
+            $this->h3 = new ModelHelicoptere(15,0,0);
         }else{
-            $this->helicopteres=[new Helicoptere(5,12,180),new Helicoptere(10,12,180),new Helicoptere(15,12,180)];
+            $this->h1 = new ModelHelicoptere(5,12,180);
+            $this->h2 = new ModelHelicoptere(10,12,180);
+            $this->h3 = new ModelHelicoptere(15,12,180);
         }
         $this->save();
     }
-    function changeDirectionHelicopters($id,$angleH1,$angleH2,$angleH3){
-        $player = ModelJoueur::getPlayer($id);
+    function changeDirectionHelicopters($angleH1,$angleH2,$angleH3){
         if($angleH1>90 || $angleH2>90 || $angleH3>90 ||$angleH1<-90 || $angleH2<-90 || $angleH3<-90){
             throw new TooLargeAngleException();
         }
-        $player->helicopteres[0]->changeDirection($angleH1);
-        $player->helicopteres[1]->changeDirection($angleH2);
-        $player->helicopteres[2]->changeDirection($angleH3);
+        $h1 = ModelHelicoptere::getHelicoptere($this->h1);
+        $h2 = ModelHelicoptere::getHelicoptere($this->h2);
+        $h3 = ModelHelicoptere::getHelicoptere($this->h3);
+
+        $h1->changeDirection($angleH1);
+        $h2->changeDirection($angleH2);
+        $h3->changeDirection($angleH3);
+
     }
 
     function moveHelicopters($distanceH1,$distanceH2,$distanceH3){
         if($distanceH1>3 || $distanceH2>3 || $distanceH3>3 ||$distanceH1<0 || $distanceH2<0 || $distanceH3<0){
             throw new InvalidDistanceException();
         }
-        $this->helicopteres[0]->move($distanceH1);
-        $this->helicopteres[1]->move($distanceH2);
-        $this->helicopteres[2]->move($distanceH3);
+
+        $h1 = ModelHelicoptere::getHelicoptere($this->h1);
+        $h2 = ModelHelicoptere::getHelicoptere($this->h2);
+        $h3 = ModelHelicoptere::getHelicoptere($this->h3);
+
+        $h1->move($distanceH1);
+        $h2->move($distanceH2);
+        $h3->move($distanceH3);
+       
     }
 
     function chooseTargets($targetH1,$targetH2,$targetH3){
