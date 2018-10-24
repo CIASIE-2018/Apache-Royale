49216241ef86bf215a0322b09505679947690662
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-10-24 09:00:52 +0200

    Modelé Helicoptére et Model Joueur

---
+++ www/src/Model/ModelHelicoptere.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace apache\Model;
+
+class ModelHelicoptere extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'Salon';
+    protected $primaryKey = 'id';
+    public $timestamps =false;
+}
