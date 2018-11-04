89ad9d078f87227b18cb5d659bb5b2357868a3d6
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-11-04 21:38:22 +0100

    js++

--- www/js/main.js
+++ www/js/main.js
@@ -1,21 +1,18 @@
-let helico={};
-
-helico.modules = {};
 
 let helicos = document.getElementsByClassName("playable");
-
-<<<<<<< Updated upstream
+let helicoactif;
 for (let index = 0; index < 3; index++) {
     helicos.item(index).onclick = function () {
+        helicoactif = document.getElementById(this.id);
+        helicoactif.style.backgroundColor= ("green");
         console.log(this.id);
     };
 }
-console.log(helicos);
-=======
-heli.forEach(elem => {
-    
-});
-heli.onclick = function(){
-    console.log("frfef")
-};
->>>>>>> Stashed changes
+
+let cases = document.getElementsByClassName("case");
+for (let i = 0; i <cases.length; i++) {
+    cases.item(i).onclick = function () {
+        
+    };
+}
+
