c242c1c2759b607847c6a92941dc3954fd50f947
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-11-04 21:13:33 +0100

    conflit

--- www/js/main.js
+++ www/js/main.js
@@ -4,9 +4,18 @@ helico.modules = {};
 
 let helicos = document.getElementsByClassName("playable");
 
+<<<<<<< Updated upstream
 for (let index = 0; index < 3; index++) {
     helicos.item(index).onclick = function () {
         console.log(this.id);
     };
 }
 console.log(helicos);
+=======
+heli.forEach(elem => {
+    
+});
+heli.onclick = function(){
+    console.log("frfef")
+};
+>>>>>>> Stashed changes
