1d0a45d7fd935e84f9e9f07d2a5dd1f1a43441a0
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-11-04 20:58:08 +0100

    petit patch

--- www/js/main.js
+++ www/js/main.js
@@ -2,8 +2,6 @@ let helico={};
 
 helico.modules = {};
 
-let search = document.getElementById("searchTxt").value;
-
 let heli = document.getElementsByClassName("playable");
 
 heli.onclick = function(){
