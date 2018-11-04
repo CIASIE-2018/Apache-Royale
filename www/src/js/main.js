9e4272cc9a7c83bedcaa14d5275d301931ed2d8a
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-11-04 20:45:03 +0100

    debut js

---
+++ www/src/js/main.js
@@ -0,0 +1,11 @@
+let helico={};
+
+helico.modules = {};
+
+let search = document.getElementById("searchTxt").value;
+
+let heli = document.getElementsByClassName("playable");
+
+heli.onclick = function(){
+    console.log("frfef")
+};
